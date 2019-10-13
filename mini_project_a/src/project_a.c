2b443a285f7fb78e5a643195c6c94b55363a5bbd
jadon <jadonwolffs@gmail.com>
2019-10-13 15:11:16 +0200

    changed array decl

--- mini_project_a/src/project_a.c
+++ mini_project_a/src/project_a.c
@@ -64,7 +64,8 @@ int main(void)
 		{
 			printf("%f %f %d\n",light,hum,dac_out);
 		}
-		unsigned char * dac_char_array = (unsigned char *) (0b0111<<12 | ((int)dac_out)<<2 | 0b00);//|0b00 isn't strictly necessary
+		unsigned char * dac_char_array[16];
+		dac_char_array = (unsigned char *) (0b0111<<12 | ((int)dac_out)<<2 | 0b00);//|0b00 isn't strictly necessary
 		printf("%s",dac_char_array);
 		wiringPiSPIDataRW(SPI_CHAN_DAC, dac_char_array, 1);
 		// RTC Time 	Sys Timer 	Humidity 	Temp 	Light 	DAC out Alarm
