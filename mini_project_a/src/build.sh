6d29d6c913dbe3d9a3064d26c201938a6e670280
jadon <jadonwolffs@gmail.com>
2019-10-13 16:02:24 +0200

    removed unneccessary lines from build

--- mini_project_a/src/build.sh
+++ mini_project_a/src/build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-
+//
 case "$1" in
 raspberry)
-    sudo apt-get update
-    sudo apt-get upgrade
-    sudo apt-get install build-essential git-core
-    git clone git://git.drogon.net/wiringPi
-    cd wiringPi
-    git pull origin
-    ./build
-    gpio -v
-    cd ..
     make clean all target=raspberry
     exit 0
     ;;
