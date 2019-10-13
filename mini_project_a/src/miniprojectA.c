894f3b1e7dffaf553e70147d037e0758cb82509d
jadon <jadonwolffs@gmail.com>
2019-10-13 14:09:23 +0200

    removed inversion of light

--- mini_project_a/src/miniprojectA.c
+++ mini_project_a/src/miniprojectA.c
@@ -53,7 +53,7 @@ int main(void)
 		// printf("Humidity: %0.1fV\n", channels[3] * 3.3 / 1023);
 		// printf("Light Level: %d\n", 1023-channels[0]);
 		int temp = round(((channels[1] * 3.3 / 1023) - 0.7) / 0.01);
-		int light = 1023-channels[0];
+		int light = channels[0];
 		// printf("Temperature: %0.0f\n", temp);
 		secs = hexCompensation(wiringPiI2CReadReg8(RTC, SEC) - 0b10000000);
 		mins = hexCompensation(wiringPiI2CReadReg8(RTC, MIN));
