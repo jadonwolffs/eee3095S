6c821f29036d16f6a52a32a16d9bd203e673cae4
jadon <jadonwolffs@gmail.com>
2019-10-13 17:26:33 +0200

    addded file io

--- mini_project_a/src/main.cpp
+++ mini_project_a/src/main.cpp
@@ -42,7 +42,7 @@ void setup()
       char temp_char[255];
       fp = fopen("temp.txt", "r");
       fscanf(fp, "%s", temp_char);
-      printf("1 : %s\n", butemp_charff );
+      printf("1 : %s\n", temp_char );
       fclose(fp);
       temp = (int) temp_char;
       Blynk.virtualWrite(V1, temp);
